e49d0e26d21bebe70977b314d230e084c7c677a8
Matthew McGrath <rubywep@hotmail.com>
2022-02-17 13:11:31 -0800

    Second pass of HTML / CSS / JS. Mostly finished!
    Completed the proper logic required for toggling between Monthly and Anual pricing via JavaScript. Styling is essentially complete, with a few minor improvements that could be made.

--- Junior Level/pricing-component-with-toggle-master/script.js
+++ Junior Level/pricing-component-with-toggle-master/script.js
@@ -1,4 +1,19 @@
-const basicPriceElement = document.querySelector("#price--pro");
-const proPriceElement = document.querySelector("#price--pro");
-const masterPriceElement = document.querySelector("#price--master");
+let checkbox = document.querySelector('input[type=checkbox]');
+let basicPriceElement = document.querySelector('#price--basic .amount-span');
+let proPriceElement = document.querySelector('#price--pro .amount-span');
+let masterPriceElement = document.querySelector('#price--master .amount-span');
 
+console.log(checkbox);
+
+checkbox.addEventListener('click', (event) => {
+  if (checkbox.checked) {
+    // Set 'Monthly' values
+    basicPriceElement.textContent = '19.99';
+    proPriceElement.textContent = '24.99';
+    masterPriceElement.textContent = '39.99';
+  } else {
+    basicPriceElement.textContent = '199.99';
+    proPriceElement.textContent = '249.99';
+    masterPriceElement.textContent = '399.99';
+  }
+});
