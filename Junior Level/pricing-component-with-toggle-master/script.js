57218a70212a6ee4663559302693263d68adea4c
Matthew McGrath <rubywep@hotmail.com>
2022-02-16 22:55:35 -0800

    Add 1st-pass of HTML. Create CSS & JS files.
    First-pass of HTML, including what I think will be the necessary classes, IDs, and elements. Create JS which grabs the 3 different pricing labels. Create CSS file which does some very basic resets.

---
+++ Junior Level/pricing-component-with-toggle-master/script.js
@@ -0,0 +1,4 @@
+const basicPriceElement = document.querySelector("#price--pro");
+const proPriceElement = document.querySelector("#price--pro");
+const masterPriceElement = document.querySelector("#price--master");
+
