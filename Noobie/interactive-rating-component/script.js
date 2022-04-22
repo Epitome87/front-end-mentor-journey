5ba12771384adf46987deadbf5b438fd2dc8c8a6
Matthew McGrath <rubywep@hotmail.com>
2022-04-21 22:50:08 -0700

    Add mobile responsiveness, refine JS logic
    Before, the user's selected rating was being submitted whenever a rating was clicked. Now, it's only submitted when the Submit button is actually clicked.

--- Noobie/interactive-rating-component/script.js
+++ Noobie/interactive-rating-component/script.js
@@ -1,18 +1,49 @@
 console.log('JavaScript connected');
 
+// We'll need to hide & un-hide these cards when appropriate
 const cardRate = document.querySelector('.card--rate');
 const cardThanks = document.querySelector('.card--thanks');
+
+// We need to dynamically change the text of this element to the selected rating
 const ratingText = document.querySelector('.rating-text');
+
+// We need to listen to clicks on all 5 of our possible rating options
 const ratingOptions = document.querySelectorAll('.rating');
 
+// We need to submit the selected rating when button is clicked
+const submitBtn = document.querySelector('button');
 
-const selectRating = (rating) => {
-    cardRate.classList.add('hide');
-    cardThanks.classList.remove('hide');
+// Helper variable -- probably more elegant ways to do this!
+let selectedRatingValue;
 
-    ratingText.textContent = `${rating}`;
-}
+// Function which will give a 'selected' class to the option that was clicked, and remove it from all others
+const selectRating = (event) => {
+  const selectedRating = event.target;
 
-ratingOptions.forEach((option, index) => {
-    option.addEventListener('click', (event) => selectRating(index + 1))
-})
+  ratingOptions.forEach((option) => {
+    option.classList.remove('selected');
+  });
+
+  // We want to set the selected rating to a 'selected' state, for styling purposes
+  selectedRating.classList.add('selected');
+
+  // Let's keep track what value this selected rating represents, for when we hit Submit!
+  selectedRatingValue = selectedRating.innerText;
+};
+
+const submitRating = (event) => {
+  // Hide the rating card, un-hide the thanks card
+  cardRate.classList.add('hide');
+  cardThanks.classList.remove('hide');
+
+  // And set the rating of the element to the selected value
+  ratingText.textContent = `${selectedRatingValue}`;
+};
+
+// Listen to our click events on all our options
+ratingOptions.forEach((option) => {
+  option.addEventListener('click', selectRating);
+});
+
+// Listen to click event on the Submit Button
+submitBtn.addEventListener('click', submitRating);
